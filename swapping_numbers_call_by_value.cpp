53f6f47276392b3a2257d6de29d2a90fe0aa94e4
Suhani Gupta <144280247+suhanigupta23@users.noreply.github.com>
2024-04-08 15:42:56 +0530

    swapping numbers call by value

---
+++ swapping_numbers_call_by_value.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//CALL BY VALUE
+void swap(int x,int y)
+{
+    int temp;
+    temp=x;
+    x=y;
+    y=temp;
+}
+int main()
+{
+    int a,b;
+    a=10;
+    b=20;
+    swap(a,b);
+    printf("%d %d",a,b);
+}
