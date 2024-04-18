8979ea0f2d3471fcf6d8f11b3481256be85deb43
Suhani Gupta <144280247+suhanigupta23@users.noreply.github.com>
2024-04-18 20:15:17 +0530

    counting number of vowels and consonants

---
+++ reading_number_of_vowels_and_consonants.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    char A[]="How are you";
+    int i;
+    int vcount=0,ccount=0;
+    for(i=0;A[i]!=0;i++)
+    {
+        if(A[i]=='a'||A[i]=='e'||A[i]=='i'||A[i]=='o'||A[i]=='u'
+       || A[i]=='A'||A[i]=='E'||A[i]=='I'||A[i]=='O'||A[i]=='U')
+        {
+            vcount++;
+        }
+        else if(A[i]>=65&&A[i]<=90||
+        A[i]>=97&&A[i]<=122)
+        {
+            ccount++;
+        }
+    }
+    printf("The number of vowels are: %d\n",vcount);
+    printf("The number of constants are: %d\n",ccount);
+}
